41b93ad98c88dc1e787849ec95858bccdcd5aee0
Vladimir <adeptius@gmail.com>
2016-09-30 20:40:44 +0300

    artifact setup

---
+++ src/main/java/ua/adeptius/java/servlets/ClockServlet.java
@@ -0,0 +1,16 @@
+package ua.adeptius.java.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class ClockServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Current time is : " + new Date());
+    }
+}
